1344f29f18b98818f5f3de1f25ee391c3fec4e51
brightjokerr <mypd96@gmail.com>
2016-10-26 23:21:12 +0530

    Add files via upload

--- main_code.py
+++ main_code.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python
+
+# import module movie to use the object Movie
 import movie
+
+# import fresh_tomatoes to display the python code as a web page
 import fresh_tomatoes
 
-Dark_knight = movie.Movie("Dark Knight 1", "How Batman turn out to save Gotham",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Dark_Knight.jpg/220px-Dark_Knight.jpg",
-                            "https://youtu.be/EXeTwQWrcwY")
+# A new object is created of Movie from the imported movie module
+Dark_knight = movie.Movie("Dark Knight 1",
+                          "How Batman turn out to save Gotham",
+                          "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Dark_Knight.jpg/220px-Dark_Knight.jpg",   # noqa
+                          "https://youtu.be/EXeTwQWrcwY")
 
 avatar = movie.Movie("Avatar",
                      "About a paralyzed soilder on planet",
-                     "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-",
+                     "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-",  # noqa
                      "https://youtu.be/5PSNL1qE6VY")
 
 perks_of_being_a_wallflower = movie.Movie("Perks of Being a Wallflower",
-                                          " About a boy who is socially different",
-                                          "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/The_Perks_of_Being_a_Wallflower_Poster.jpg/215px-The_Perks_of_Being_a_Wallflower_Poster.jpg",
+                                          " About a boy who is socially different",  # noqa
+                                          "https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/The_Perks_of_Being_a_Wallflower_Poster.jpg/215px-The_Perks_of_Being_a_Wallflower_Poster.jpg",   # noqa
                                           "https://youtu.be/n5rh7O4IDc0")
 
 three_idiots = movie.Movie("3 Idiots",
-                        "About life of three friends in a esteemed engineering college",
-                        "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/3_idiots_poster.jpg/220px-3_idiots_poster.jpg",
-                        "https://youtu.be/K0eDlFX9GMc")
+                           "About life of three friends in a esteemed engineering college",   # noqa
+                           "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/3_idiots_poster.jpg/220px-3_idiots_poster.jpg",   # noqa
+                           "https://youtu.be/K0eDlFX9GMc")
+
+# Names of the movies are stored in a list
 movies = [Dark_knight, avatar, perks_of_being_a_wallflower, three_idiots]
+
+# List is passed on to the function in frest_tomatoes so that they can be displayed as a web page  # noqa
 fresh_tomatoes.open_movies_page(movies)
--- movie.py
+++ movie.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python
 import webbrowser
 
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """
+    Class Movie defines the movie block shown on the website.
+    It defines the information that the website holds for the particular movie
+    like Name of the movie, stroyline in brief ,corresponding image followed by the trailer url. # noqa
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):  # noqa
+        """
+        The constructor __init__ takes the parameters like Name of the movie,
+        Brief storyline, the image url and later the trailer url .
+        The values passed are used to initialize the variables inside the class
+         """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        This particular funtion is used to call the open function from the
+        webbrowser module. This helps us to stream the trailer for the movie.
+        """
         webbrowser.open(self.trailer_youtube_url)
